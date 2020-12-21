a03bb6bdfdf115951fbff023cc615270a661af3c
Ivan Babrou <github@ivan.computer>
2020-12-20 17:24:44 -0800

    Enable /lib/docker and /libexec/docker as plugin paths
    This is useful in Nix, where /usr doesn't really exist.

--- components/cli/cli-plugins/manager/manager_unix.go
+++ components/cli/cli-plugins/manager/manager_unix.go
@@ -3,6 +3,10 @@
 package manager
 
 var defaultSystemPluginDirs = []string{
-	"/usr/local/lib/docker/cli-plugins", "/usr/local/libexec/docker/cli-plugins",
-	"/usr/lib/docker/cli-plugins", "/usr/libexec/docker/cli-plugins",
+	"/usr/local/lib/docker/cli-plugins",
+	"/usr/local/libexec/docker/cli-plugins",
+	"/usr/lib/docker/cli-plugins",
+	"/usr/libexec/docker/cli-plugins",
+	"/lib/docker/cli-plugins",
+	"/libexec/docker/cli-plugins",
 }
